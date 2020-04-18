3c2b75afc90fb440380f03764f0cf683ea474486
JulioSolano116 <jcsolano61@misena.edu.co>
2020-04-17 21:34:00 -0500

    punto control 5

---
+++ pruebacambios1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1, n2, suma;
+	cout<<"Digite un numero\n";
+	cin>>n1;
+	cout<<"Digie otro numero\n";
+	cin>>n2;
+	suma = n1 + n2;
+	
+	return 0;
+}
